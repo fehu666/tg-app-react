1a84c16f21934b13a1329921996f1d0c98179fdf
Vladislav <marazm14@gmail.com>
2023-02-18 00:33:52 +0300

    ver 8

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -11,7 +11,7 @@ const Header = (props) => {
         <div className={'header'}>
             <Button onClick={onClose}>Закрыть</Button>
             <span className={'username'}>
-                {user?.username}
+                {user?.first_name}
             </span>
         </div>
     );
