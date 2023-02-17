fe8da04537445771baf65d822b3a27fa3f6eafeb
Vladislav <marazm14@gmail.com>
2023-02-18 00:31:35 +0300

    ver 7

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { useEffect } from 'react';
 import './App.css';
+import Header from './components/Header/Header';
 import { useTelegram } from './hooks/useTelegram';
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
 
   return (
     <div className="App">
+      <Header />
       <button onClick={onToggleButton}>Toggle</button>
     </div>
   );
